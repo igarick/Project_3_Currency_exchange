6e57af418523b641b096e4cad1409dbfb00f79ce
Igor Work <keaerta@gmail.com>
2025-09-23 15:02:29 +0500

    change DAO with Lombok

--- src/main/java/entities/Currency.java
+++ src/main/java/entities/Currency.java
@@ -1,74 +1,17 @@
 package entities;
 
-import java.util.Objects;
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
 
 public class Currency {
     private Long id;
     private String code;
     private String fullName;
     private String sign;
-
-    public Currency(Long id, String code, String fullName, String sign) {
-        this.id = id;
-        this.code = code;
-        this.fullName = fullName;
-        this.sign = sign;
-    }
-
-    public Currency() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getSign() {
-        return sign;
-    }
-
-    public void setSign(String sign) {
-        this.sign = sign;
-    }
-
-    @Override
-    public String toString() {
-        return "Currencies{" +
-               "id=" + id +
-               ", code='" + code + '\'' +
-               ", name='" + fullName + '\'' +
-               ", sign='" + sign + '\'' +
-               '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Currency currency = (Currency) o;
-        return Objects.equals(id, currency.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
 }
--- src/main/java/entities/ExchangeRate.java
+++ src/main/java/entities/ExchangeRate.java
@@ -1,75 +1,21 @@
 package entities;
 
+import lombok.*;
+
 import java.math.BigDecimal;
 import java.util.Objects;
+
+
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
 
 public class ExchangeRate {
     private Long id;
     private Currency baseCurrencyId;
     private Currency targetCurrencyId;
     private BigDecimal rate;
-
-    public ExchangeRate(Long id, Currency baseCurrencyId, Currency targetCurrencyId, BigDecimal rate) {
-        this.id = id;
-        this.baseCurrencyId = baseCurrencyId;
-        this.targetCurrencyId = targetCurrencyId;
-        this.rate = rate;
-    }
-
-    public ExchangeRate() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Currency getBaseCurrencyId() {
-        return baseCurrencyId;
-    }
-
-    public void setBaseCurrencyId(Currency baseCurrencyId) {
-        this.baseCurrencyId = baseCurrencyId;
-    }
-
-    public Currency getTargetCurrencyId() {
-        return targetCurrencyId;
-    }
-
-    public void setTargetCurrencyId(Currency targetCurrencyId) {
-        this.targetCurrencyId = targetCurrencyId;
-    }
-
-    public BigDecimal getRate() {
-        return rate;
-    }
-
-    public void setRate(BigDecimal rate) {
-        this.rate = rate;
-    }
-
-    @Override
-    public String toString() {
-        return "ExchangeRate{" +
-               "ID=" + id +
-               ", BaseCurrencyId='" + baseCurrencyId + '\'' +
-               ", TargetCurrencyId='" + targetCurrencyId + '\'' +
-               ", Rate=" + rate +
-               '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        ExchangeRate that = (ExchangeRate) o;
-        return id == that.id && Objects.equals(baseCurrencyId, that.baseCurrencyId) && Objects.equals(targetCurrencyId, that.targetCurrencyId) && Objects.equals(rate, that.rate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, baseCurrencyId, targetCurrencyId, rate);
-    }
 }
